5467e970c24caf2dce6284621f2fc5fa35454de4
MNnazrul <mnnazrulislam196130@gmail.com>
2023-12-03 00:59:14 +0600

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import "dotenv/config";
+import mongoose, { mongo } from "mongoose";
+
 const app = express();
 const PORT = process.env.PORT || 3002;
+const mongodbURL = process.env.mongoURL;
 
 app.get("/", (req, res) => {
   res.json("working");
 });
 
-app.listen(PORT, () => {
-  console.log(`running on ${PORT}`);
-});
+mongoose
+  .connect(mongodbURL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`running on ${PORT}`);
+      console.log(`Mongodb is connected`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
