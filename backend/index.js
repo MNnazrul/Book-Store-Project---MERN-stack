f6917eafcbbd35f07d99b4ad7c3c277ae46082ad
MNnazrul <mnnazrulislam196130@gmail.com>
2023-12-02 19:29:23 +0600

    new com

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const PORT = require("./config.js");
-
-const app = express();
-
-app.listen(PORT, () => {
-  console.log(`App is running to port : ${PORT}`);
-});
