a50ce20f9b4b752b247585245e2566ed9ebd7164
MNnazrul <mnnazrulislam196130@gmail.com>
2023-12-04 00:07:34 +0600

    create routes

--- backend/index.js
+++ backend/index.js
@@ -2,31 +2,18 @@ import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModels.js";
+import BookRouter from "./routes/booksRoute.js";
 const app = express();
 const PORT = process.env.PORT || 3002;
 const mongodbURL = process.env.mongoURL;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("working");
 });
 
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return req.status(300).send({
-        message: "Send all required fields: title, author, publishYear",
-      });
-    }
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send({ message: err.message });
-  }
-});
+app.use("/books", BookRouter);
 
 mongoose
   .connect(mongodbURL)
