1fe7d88a7a4141440c29d8b399d37bf9f7ceb837
MNnazrul <mnnazrulislam196130@gmail.com>
2023-12-03 19:08:07 +0600

    newBook

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,31 @@
 import express from "express";
 import "dotenv/config";
-import mongoose, { mongo } from "mongoose";
-
+import mongoose from "mongoose";
+import { Book } from "./models/bookModels.js";
 const app = express();
 const PORT = process.env.PORT || 3002;
 const mongodbURL = process.env.mongoURL;
 
 app.get("/", (req, res) => {
   res.json("working");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return req.status(300).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
 });
 
 mongoose
